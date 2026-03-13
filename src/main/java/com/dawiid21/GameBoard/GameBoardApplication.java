e209520e79d7ae18ac65e0a343d63f6e7ff287c8
DawidHr <dawiid21@gmail.com>
2026-03-13 20:22:40 +0100

    Simple app

--- src/main/java/com/dawiid21/GameBoard/GameBoardApplication.java
+++ src/main/java/com/dawiid21/GameBoard/GameBoardApplication.java
@@ -1,5 +1,6 @@
 package com.dawiid21.GameBoard;
 
+import com.dawiid21.GameBoard.model.MainBoard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GameBoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GameBoardApplication.class, args);
+		MainBoard mainBoard = new MainBoard();
+		mainBoard.start();
+		//SpringApplication.run(GameBoardApplication.class, args);
 	}
 
 }
