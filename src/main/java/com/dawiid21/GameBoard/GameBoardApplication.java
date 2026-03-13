6759a53cfe01ed3022a4b075f3a47047b32de431
Cool <dawiid21@gmail.com>
2026-03-13 21:30:42 +0100

    Move all methods to ConsoleUi and delete MainBoard class

--- src/main/java/com/dawiid21/GameBoard/GameBoardApplication.java
+++ src/main/java/com/dawiid21/GameBoard/GameBoardApplication.java
@@ -1,16 +1,13 @@
 package com.dawiid21.GameBoard;
 
-import com.dawiid21.GameBoard.model.MainBoard;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GameBoardApplication {
 
 	public static void main(String[] args) {
-		MainBoard mainBoard = new MainBoard();
-		mainBoard.start();
-		//SpringApplication.run(GameBoardApplication.class, args);
+		ConsoleUI consoleUI = new ConsoleUI();
+		consoleUI.start();
 	}
 
 }
