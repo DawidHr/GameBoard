34e2eac5f1322ef57329868b3f903f3e45a60050
DawidHr <dawiid21@gmail.com>
2026-03-16 21:30:07 +0100

    add updateScoreTest and getSummaryGamesTest

--- src/test/java/com/dawiid21/GameBoard/GameServiceTest.java
+++ src/test/java/com/dawiid21/GameBoard/GameServiceTest.java
@@ -1,6 +1,9 @@
 package com.dawiid21.GameBoard;
 
+import com.dawiid21.GameBoard.model.Game;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,5 +40,50 @@ class GameServiceTest {
         long gameId = -1L;
         gameService.finishGame(gameId);
         assertEquals(1, gameService.getGamesInProgress().size());
+    }
+
+    @Test
+    void updateScoreTest() {
+        gameService = new GameService();
+        gameService.addGame("Team A", "Team B");
+        gameService.updateScore(12, 1,1);
+        assertEquals(1, gameService.getGamesInProgress().size());
+        Game game = gameService.getGames().get(0);
+        assertEquals(0, game.getHomeTeamScore());
+        assertEquals(0, game.getAwayTeamScore());
+        gameService.updateScore(game.getGameId(), 5,1);
+        assertEquals(0, game.getHomeTeamScore());
+        assertEquals(0, game.getAwayTeamScore());
+        gameService.updateScore(game.getGameId(), 1,1);
+        assertEquals(1, game.getHomeTeamScore());
+        assertEquals(0, game.getAwayTeamScore());
+        gameService.updateScore(game.getGameId(), 2,1);
+        assertEquals(1, game.getHomeTeamScore());
+        assertEquals(1, game.getAwayTeamScore());
+    }
+
+    @Test
+    void getSummaryGamesTest() {
+        gameService = new GameService();
+        gameService.addGame("Team A", "Team B");
+        gameService.addGame("Team A 1", "Team B 1");
+        gameService.addGame("Team A 2", "Team B 2");
+        Game game1 = gameService.getGames().get(0);
+        gameService.finishGame(game1.getGameId());
+        Game game2 = gameService.getGames().get(1);
+        gameService.updateScore(game2.getGameId(), 1, 1);
+        gameService.updateScore(game2.getGameId(), 2, 1);
+        gameService.updateScore(game2.getGameId(), 1, 1);
+        gameService.finishGame(game2.getGameId());
+        Game game3 = gameService.getGames().get(2);
+        gameService.updateScore(game3.getGameId(), 1, 1);
+        gameService.finishGame(game3.getGameId());
+        gameService.addGame("Team A 3", "Team B 3");
+
+        LinkedList<Game> results = gameService.getSummaryGames();
+        assertEquals(3, results.size());
+        assertEquals("Team A 1", results.get(0).getHomeTeam());
+        assertEquals("Team A 2", results.get(1).getHomeTeam());
+
     }
 }
